29e5bac04e6d9764b1dffce25004cffa5cac5247
Kiattiyot Leaman <kl@Kiattiyots-MacBook-Air.local>
2022-12-05 11:21:22 +0700

    Add Google Analytic

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,33 @@
 import '../styles/globals.css'
 import MainLayout from "../components/layout/MainLayout"
 
+import Script from 'next/script'
+
 function MyApp({ Component, pageProps }) {
   return (
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=UA-250984486-1`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-250984486-1', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
   )
 }
 
