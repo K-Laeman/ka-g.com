e0568233e9ecb99c0f03247664452e314704d698
Kiattiyot Leaman <kl@Kiattiyots-MacBook-Air.local>
2022-12-06 11:35:41 +0700

    Fixed Google Tag Script

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,30 @@
 import '../styles/globals.css'
 import MainLayout from "../components/layout/MainLayout"
-
 import Script from 'next/script'
 
+const GA_MEASUREMENT_ID = 'UA-250984486-1';
+
 function MyApp({ Component, pageProps }) {
+
   return (
     <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
-        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=UA-250984486-1`}
-      />
-      <Script
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-250984486-1', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
       />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-250984486-1');
+        `}
+      </Script>
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
     </>
+
   )
 }
 
